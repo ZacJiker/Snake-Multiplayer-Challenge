254d67f8c77069eb933be27ab8397b061deb7efc
SAUVECANNE, Baptiste <baptiste.sauvecanne@elysiumaircraft.com>
2024-10-03 16:35:24 +0200

    feature: update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="snake_multiplayer_challenge",
-    version="0.1",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=[
         "pygame>=2.0.0",
